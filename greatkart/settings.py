9ca66f9a4eba41f807f65598abf7da8b45ae91d5
NHEM MONEAKSEKA <moneaksekanhem2016@gmail.com>
2024-09-16 09:36:26 +0700

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -133,3 +133,16 @@ STATICFILES_DIRS = [
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+# SMTP configuration
+EMAIL_HOST = 'mail.princeconsumer.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'itmanager@princeconsumer.com'
+EMAIL_HOST_PASSWORD = 'A123456a'
+EMAIL_USE_TLS = False
